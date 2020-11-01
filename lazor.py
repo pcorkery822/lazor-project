3af7b732d639fe32036f8da2b997fbf830f124e5
pcorkery822 <72817994+pcorkery822@users.noreply.github.com>
2020-11-01 16:33:07 -0500

    Add files via upload

--- lazor.py
+++ lazor.py
@@ -1,3 +1,28 @@
+import copy
+
+
+class Blocks:
+
+    def __init__(self, type):
+        self.type = type
+
+    def interact(self, other, position):
+        if position[0] % 2 == 1:
+            axis = 4
+        elif position[1] % 2 == 1:
+            axis = 3
+ 
+        if self.type == 'reflect':
+            other[axis] = -other[axis]
+        elif self.type == 'refract':
+            new = copy.deepcopy(other)
+            other[axis] = -other[axis]
+            other.append(new)
+        elif self.type == 'opaque':
+            other[3] = 0
+            other[4] = 0
+
+
 def load_file(fptr):
     '''
     :param fptr: Input file that contains grid, laser positions, number of movable blocks and their types, and target
@@ -16,7 +41,9 @@ def load_file(fptr):
     grid = []
     start = False
     # Initialize list of movable blocks
-    blocks = [0, 0, 0]
+    refract = []
+    reflect = []
+    opaque = []
     # Initialize list for laser positions and directions
     laser = []
     # Initialize list for target points
@@ -34,11 +61,17 @@ def load_file(fptr):
         if len(line) > 0:
             # Read movable blocks from file
             if line[0] == 'A' and not start:
-                blocks[0] = int(line[1:])
+                num = int(line[1:])
+                for i in range(num):
+                    reflect.append(1)
             elif line[0] == 'B' and not start:
-                blocks[1] = int(line[1:])
+                num = int(line[1:])
+                for i in range(num):
+                    refract.append(1)
             elif line[0] == 'C' and not start:
-                blocks[2] = int(line[1:])
+                num = int(line[1:])
+                for i in range(num):
+                    opaque.append(1)
             # Read laser positions and directions from file
             elif line[0] == 'L':
                 line = line[1:]
@@ -48,10 +81,53 @@ def load_file(fptr):
                 line = line[1:]
                 points.append([int(i) for i in line.split()])
 
+    blocks = [reflect, refract, opaque]
+
+    # Convert x's, o's, and fixed blocks to numbers in grid
+    # x = 0, o = 1, A = 2, B = 3, C = 4
+    # Initialize new grid with 2n+1 x 2n+1 indices where original grid was n x n
+    full_grid = []
+    for y, line in enumerate(grid):
+        row = []
+        zeros = []
+        for z in range(2 * len(line) + 1):
+            row.append(0)
+            zeros.append(0)
+        if y == 0:
+            full_grid.append(zeros)
+        full_grid.append(row)
+        full_grid.append(zeros)
+
+    # Initialize list to store potential positions for movable blocks
+    empty = []
+    for y, line in enumerate(grid):
+        for z, ind in enumerate(line):
+            if ind == 'x':
+                full_grid[2 * y + 1][2 * z + 1] = 'Unavailable'
+            elif ind == 'o':
+                full_grid[2 * y + 1][2 * z + 1] = 'Open'
+                empty.append([2 * y + 1, 2 * z + 1])
+            elif ind == 'A':
+                full_grid[2 * y + 1][2 * z + 1] = 'Reflect'
+            elif ind == 'B':
+                full_grid[2 * y + 1][2 * z + 1] = 'Opaque'
+            elif ind == 'C':
+                full_grid[2 * y + 1][2 * z + 1] = 'Refract'
+
     bff_file.close()
-    return grid
+    return full_grid, empty, laser, blocks, points
+
+
+def solve_puzzle(grid, empty, laser, blocks, points):
+
+    for i, j in enumerate(empty):
+        grid_new = grid
+
+
+
 
 
 if __name__ == '__main__':
-    puzzle = load_file('dark_1.bff')
-    print(puzzle)
+    puzzle, positions, lasers, movable_blocks, targets = load_file('dark_1.bff')
+    solution = solve_puzzle(puzzle, positions, lasers, movable_blocks, targets)
+
